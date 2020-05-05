73ab4f1330a4aa769eaec3a0ca6341d16e71f531
youfu <youfufu16@gmail.com>
2020-05-05 07:11:53 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>様々な個性を発見しよう</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name }}ログイン中
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>様々な個性を発見しよう</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
