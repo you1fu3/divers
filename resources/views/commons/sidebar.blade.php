1437c6625e4190a8691c363242da912f797c69f5
youfu <youfufu16@gmail.com>
2020-05-05 06:52:38 +0000

    user registration

---
+++ resources/views/commons/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class ="row flex-xl-nowrap">
+    <div class="col-sm-3">
+        <nav class="nav flex-column">
+            
+            <div class="collapse" id="side-nav">
+                <ul class="navbar-nav">
+                    <li><a class="nav-link active" href="#">Active</a></li>
+                    <li><a class="nav-link" href="#">Link</a></li>
+                    <li><a class="nav-link" href="#">Link</a></li>
+                    <li><a class="nav-link disabled" href="#">Disabled</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#side-nav" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-target
+  </button>
+        メインフィールド
+    </div>
+</div>
