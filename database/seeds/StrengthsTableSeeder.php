169feb7e8f614b317652a11adb6ecb3c7f895078
youfu <youfufu16@gmail.com>
2020-05-10 12:22:15 +0000

    Seeder

---
+++ database/seeds/StrengthsTableSeeder.php
@@ -0,0 +1,151 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StrengthsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '分析思考'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '着想'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '学習欲'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '原点思考'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '収集心'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '戦略性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '未来思考'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '戦略的思考力',
+            'strength' => '内省'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '達成欲'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '公平性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '目標志向'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => 'アレンジ'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '慎重さ'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '責任感'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '信念'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '規律性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '実行力',
+            'strength' => '回復志向'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => '活発性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => '競争性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => '自我'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => '指令性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => '最上志向'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => '社交性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => 'コミュニケーション'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '影響力',
+            'strength' => '自己確信'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '適応性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '共感性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '個別化'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '運命思考'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '調和性'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => 'ポジティブ'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '成長促進'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '包含'
+        ]);
+        DB::table('strengths')->insert([
+            'type' => '人間関係構築力',
+            'strength' => '親密性'
+        ]);
+    }
+}
